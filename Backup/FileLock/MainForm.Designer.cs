0acb4e8e5103f67a8c374b513825787b6700e5eb
Edward <edward_song@yeah.net>
2018-08-21 11:01:29 +0800

    1st commit

---
+++ Backup/FileLock/MainForm.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace FileLock
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInFile = new System.Windows.Forms.Button();
+            this.txtInFile = new System.Windows.Forms.TextBox();
+            this.txtPassWord = new System.Windows.Forms.TextBox();
+            this.btnEncy = new System.Windows.Forms.Button();
+            this.btnDecy = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnInFile
+            // 
+            this.btnInFile.Location = new System.Drawing.Point(205, 25);
+            this.btnInFile.Name = "btnInFile";
+            this.btnInFile.Size = new System.Drawing.Size(75, 23);
+            this.btnInFile.TabIndex = 0;
+            this.btnInFile.Text = "目标文件";
+            this.btnInFile.UseVisualStyleBackColor = true;
+            this.btnInFile.Click += new System.EventHandler(this.btnInFile_Click);
+            // 
+            // txtInFile
+            // 
+            this.txtInFile.AllowDrop = true;
+            this.txtInFile.Location = new System.Drawing.Point(13, 25);
+            this.txtInFile.Name = "txtInFile";
+            this.txtInFile.ReadOnly = true;
+            this.txtInFile.Size = new System.Drawing.Size(186, 21);
+            this.txtInFile.TabIndex = 1;
+            this.txtInFile.DragDrop += new System.Windows.Forms.DragEventHandler(this.txtInFile_DragDrop);
+            this.txtInFile.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtInFile_KeyPress);
+            this.txtInFile.DragEnter += new System.Windows.Forms.DragEventHandler(this.txtInFile_DragEnter);
+            // 
+            // txtPassWord
+            // 
+            this.txtPassWord.Location = new System.Drawing.Point(13, 68);
+            this.txtPassWord.Name = "txtPassWord";
+            this.txtPassWord.PasswordChar = '*';
+            this.txtPassWord.Size = new System.Drawing.Size(186, 21);
+            this.txtPassWord.TabIndex = 4;
+            // 
+            // btnEncy
+            // 
+            this.btnEncy.Enabled = false;
+            this.btnEncy.Location = new System.Drawing.Point(13, 107);
+            this.btnEncy.Name = "btnEncy";
+            this.btnEncy.Size = new System.Drawing.Size(75, 23);
+            this.btnEncy.TabIndex = 5;
+            this.btnEncy.Text = "加密";
+            this.btnEncy.UseVisualStyleBackColor = true;
+            this.btnEncy.Click += new System.EventHandler(this.btnEncy_Click);
+            // 
+            // btnDecy
+            // 
+            this.btnDecy.Enabled = false;
+            this.btnDecy.Location = new System.Drawing.Point(117, 107);
+            this.btnDecy.Name = "btnDecy";
+            this.btnDecy.Size = new System.Drawing.Size(75, 23);
+            this.btnDecy.TabIndex = 6;
+            this.btnDecy.Text = "解密";
+            this.btnDecy.UseVisualStyleBackColor = true;
+            this.btnDecy.Click += new System.EventHandler(this.btnDecy_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(206, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 44);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "密码，解密时也是用此密码";
+            // 
+            // MainForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 140);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDecy);
+            this.Controls.Add(this.btnEncy);
+            this.Controls.Add(this.txtPassWord);
+            this.Controls.Add(this.txtInFile);
+            this.Controls.Add(this.btnInFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.Text = "文件加密解密";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainForm_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.MainForm_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInFile;
+        private System.Windows.Forms.TextBox txtInFile;
+        private System.Windows.Forms.TextBox txtPassWord;
+        private System.Windows.Forms.Button btnEncy;
+        private System.Windows.Forms.Button btnDecy;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FileLock/MainForm.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FileLock
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private static string txtOutFile = "";
+        private static string extension = "";
+        private void btnInFile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.ShowDialog();
+                if (ofd.FileName != null)
+                {
+                    extension = System.IO.Path.GetExtension(ofd.FileName);//扩展名
+                    if (extension == ".encrypted")
+                    {
+                        btnEncy.Enabled = false;
+                        btnDecy.Enabled = true;
+                    }
+                    else
+                    {
+                        btnEncy.Enabled = true;
+                        btnDecy.Enabled = false;
+                    }
+                    txtInFile.Text = ofd.FileName;
+
+                }
+            }
+        }
+
+        private void btnEncy_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtOutFile = txtInFile.Text + ".encrypted";
+                CryptoHelp.EncryptFile(txtInFile.Text, txtOutFile, txtPassWord.Text.Trim());
+                MessageBox.Show("加密成功！");
+                ClearTextBox();
+            }
+            catch (CryptoHelpException che)
+            {
+                MessageBox.Show(che.Message);
+            }
+        }
+
+        private void btnDecy_Click(object sender, EventArgs e)
+        {
+
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            {
+                sfd.Title = "请选择解密文件保存位置";
+                extension = System.IO.Path.GetExtension(txtInFile.Text.Replace(".encrypted", ""));//扩展名
+                sfd.Filter = "" + extension + "|*" + extension;
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+
+                    txtOutFile = sfd.FileName;
+                    try
+                    {
+                        CryptoHelp.DecryptFile(txtInFile.Text, txtOutFile, txtPassWord.Text.Trim());
+                        MessageBox.Show("解密成功！");
+                        ClearTextBox();
+                    }
+                    catch (CryptoHelpException che)
+                    {
+                        MessageBox.Show(che.Message);
+                    }
+                }
+                else
+                {
+                }
+            }
+
+        }
+
+        private void ClearTextBox()
+        {
+            foreach (Control c in this.Controls)
+            {
+                if (c is TextBox)
+                {
+                    c.Text = "";
+                }
+                else if (c is Button)
+                {
+                    c.Enabled = false;
+                }
+            }
+            btnInFile.Enabled = true;
+        }
+
+        private void txtInFile_DragDrop(object sender, DragEventArgs e)
+        {
+            string path = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            extension = System.IO.Path.GetExtension(path);//扩展名
+            if (extension == ".encrypted")
+            {
+                btnEncy.Enabled = false;
+                btnDecy.Enabled = true;
+            }
+            else
+            {
+                btnEncy.Enabled = true;
+                btnDecy.Enabled = false;
+            }
+            txtInFile.Text = path;
+        }
+
+        private void txtInFile_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            txtInFile.Text = "";
+        }
+
+        private void txtInFile_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Move;
+            else e.Effect = DragDropEffects.None;
+
+        }
+
+        private void MainForm_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Move;
+            else e.Effect = DragDropEffects.None;
+        }
+
+        private void MainForm_DragDrop(object sender, DragEventArgs e)
+        {
+            string path = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            extension = System.IO.Path.GetExtension(path);//扩展名
+            if (extension == ".encrypted")
+            {
+                btnEncy.Enabled = false;
+                btnDecy.Enabled = true;
+            }
+            else
+            {
+                btnEncy.Enabled = true;
+                btnDecy.Enabled = false;
+            }
+            txtInFile.Text = path;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            string command = Environment.CommandLine;//获取进程命令行参数
+            string[] para = command.Split('\"');
+            if (para.Length > 4)
+            {
+                if (para[3].Length > 25)
+                {
+                    extension = System.IO.Path.GetExtension(para[3]);//扩展名
+                    if (extension == ".encrypted")
+                    {
+                        btnEncy.Enabled = false;
+                        btnDecy.Enabled = true;
+                    }
+                    else
+                    {
+                        btnEncy.Enabled = true;
+                        btnDecy.Enabled = false;
+                    }
+                    //txtInFile.Text = path;
+                    txtInFile.Text = para[3];//第四个参数是该文件的地址
+                }
+            }
+        }
+    }
+}
